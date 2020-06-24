ff03c11aca3919d630c1ca5c4763cd10e142301f
tonmoycsedu <tonmoycsedu@gmail.com>
2020-06-23 22:01:53 -0400

    reorganized code

--- js/utility.js
+++ js/utility.js
@@ -14,61 +14,10 @@ function initializePC(){
     updatePC(d,"brushed")
   })
 }
-function updatePC(data,canvas_layer){
-  if(data.length <75){
-    hideAxis = false
-    updateWordAxis(data,canvas_layer)
-  }
-  else{
-    hideAxis =  true
-    $("#word_axis").empty();
-    d3.select("#word_axis").append("foreignObject")
-                    .attr("width",200)
-                    .attr("height",200)
-                    .attr("y",pc.h()/2)
-                    .attr("x",-100)
-                    // .style("width","100px")
-                    .append("xhtml")
-                    .html("<h4>Word axis is currently not available</h4>"+
-                      "<p> as number of words > 100. "+
-                      "Use brushing, hover, search bar, or histogram on the left to select words.</p>")
-  }
-}
-function highlightWords(word,neighbors=[]){
-  this.neighbors = neighbors
-  data_rows = this.data.filter(function(d,i){return d.word == word || neighbors.includes(d.word.toLowerCase())})
-  if(!afterHighlight){
-    selected_word = word
-    pc.highlight(data_rows)
-    addWords(data_rows,neighbors)
-    $("#neighbors_list").empty()
-    neighbors.forEach(function(neighbor,i){
-        $("#neighbors_list").append('<li class="list-group-item">'+neighbor+'</li>')
-    })
-  }
-  else{
-    pc.afterHighlight(data_rows)
-    showText(data_rows,word)
-  }
-  
-}
-function cancelHighlight(){
-  if(afterHighlight){
-    afterHighlight = false
-    pc.unAfterHighlight()
-  }
-  else{
-    $("text").removeClass("focused")
-    $("text").removeAttr("fill")
-    $("#neighbors_list").empty()
-    $("#word_axis .tick text").attr("opacity","1")
-    pc.unhighlight()
-  }  
-}
 
 // fetch and replot parallel coordiante
 function onChangeHistogram(ranges=[]) {
-  console.log(ranges)
+  // console.log(ranges)
   hist_type = $("#histogram_type").val();
   // create parallel plot
   $.ajax({
@@ -95,37 +44,37 @@ function onChangeHistogram(ranges=[]) {
 
 /*Word search and highlight functions
 */
-function showText(data_rows, word){
-  attr = "gender" 
-  ctx = afterHighlight ? pc.ctx['after_highlight']:pc.ctx['highlight']
-  x = pc.position(attr)
-  y1 = 2
-  data_rows.forEach(function(row){
-    color = (row['word'] == word) ? "#43a2ca" : "orange"
-    y = pc.dimensions()[attr].yscale(row[attr]) 
-    if (typeof y == 'undefined' & !hideAxis){
-      row["x"] = x-10
-      row["color"] = color
-      row["y"] = y1+3
-      if(!afterHighlight){
-        ctx.strokeStyle = color
-        drawLine(ctx, x, y1, pc.position("gender"), pc.dimensions()["gender"].yscale(row["gender"]))
-      }
-      else{
-        ctx.strokeStyle = "orange"
-        drawLine(ctx, x, globalY, pc.position("gender"), pc.dimensions()["gender"].yscale(row["gender"]))
-      }
-      y1 += 20
-    }
-    else if((row['word'] != word)  || hideAxis){
-      row["x"] = x-10
-      row["color"] = color
-      row["y"] = y+3 
-    }
-  })
-  if(!afterHighlight)
-    addLabels(data_rows,"dynamicLabel")
-}
+// function showText(data_rows, word){
+//   attr = "gender" 
+//   ctx = afterHighlight ? pc.ctx['after_highlight']:pc.ctx['highlight']
+//   x = pc.position(attr)
+//   y1 = 2
+//   data_rows.forEach(function(row){
+//     color = (row['word'] == word) ? "#43a2ca" : "orange"
+//     y = pc.dimensions()[attr].yscale(row[attr]) 
+//     if (typeof y == 'undefined' & !hideAxis){
+//       row["x"] = x-10
+//       row["color"] = color
+//       row["y"] = y1+3
+//       if(!afterHighlight){
+//         ctx.strokeStyle = color
+//         drawLine(ctx, x, y1, pc.position("gender"), pc.dimensions()["gender"].yscale(row["gender"]))
+//       }
+//       else{
+//         ctx.strokeStyle = "orange"
+//         drawLine(ctx, x, globalY, pc.position("gender"), pc.dimensions()["gender"].yscale(row["gender"]))
+//       }
+//       y1 += 20
+//     }
+//     else if((row['word'] != word)  || hideAxis){
+//       row["x"] = x-10
+//       row["color"] = color
+//       row["y"] = y+3 
+//     }
+//   })
+//   if(!afterHighlight)
+//     addLabels(data_rows,"dynamicLabel")
+// }
 
 
 function searchWords(word){
--- js/wordAxis.js
+++ js/wordAxis.js
@@ -8,9 +8,16 @@ function addWords(data_rows,neighbors) {
 
 }
 
-function updateWordAxis(data,canvas_layer="foreground"){
-  words = data.map(function (d) {return d.word})
+/*
+Word axis update functions
+*/
+function updateWordAxisDomain(data){ // set word axis domain
   wordAxis.domain(words) 
+}
+
+function updateWordAxis(data,canvas_layer="foreground"){  // word axis update
+  words = data.map(function (d) {return d.word})
+  updateWordAxisDomain(words)
   $("#word_axis").empty()
   var yAxis = d3.svg.axis()
     .scale(wordAxis)
@@ -21,15 +28,82 @@ function updateWordAxis(data,canvas_layer="foreground"){
   drawWordLines(data,canvas_layer)
 }
 
+function updatePC(data,canvas_layer){
+  if(data.length <75){
+    hideAxis = false
+    updateWordAxis(data,canvas_layer)
+  }
+  else{
+    hideAxis =  true
+    $("#word_axis").empty();
+    d3.select("#word_axis")
+      .append("foreignObject")
+      .attr("width",200)
+      .attr("height",200)
+      .attr("y",pc.h()/2)
+      .attr("x",-100)
+      .append("xhtml")
+      .html("<h4>Word axis is currently not available</h4>"+
+        "<p> as number of words > 100. "+
+        "Use brushing, hover, search bar, or histogram on the left to select words.</p>")
+  }
+}
+
+/*
+Highlight and unhighlight functions
+*/
+function highlightWords(word,neighbors=[]){
+  this.neighbors = neighbors
+  data_rows = this.data.filter(function(d,i){return d.word == word || neighbors.includes(d.word.toLowerCase())})
+  if(!afterHighlight){
+    selected_word = word
+    pc.highlight(data_rows)
+    addWords(data_rows,neighbors)
+    $("#neighbors_list").empty()
+    neighbors.forEach(function(neighbor,i){
+        $("#neighbors_list").append('<li class="list-group-item">'+neighbor+'</li>')
+    })
+  }
+  else{
+    pc.afterHighlight(data_rows)
+    showText(data_rows,word)
+  }
+  
+}
+function cancelHighlight(){
+  if(afterHighlight){
+    afterHighlight = false
+    pc.unAfterHighlight()
+  }
+  else{
+    $("text").removeClass("focused")
+    $("text").removeAttr("fill")
+    $("#neighbors_list").empty()
+    $("#word_axis .tick text").attr("opacity","1")
+    pc.unhighlight()
+  }  
+}
+
+/*
+canvas drawing utility functions
+*/
+function drawLine(ctx,x1,y1,x2,y2){ //draw a single line
+  ctx.beginPath();
+  ctx.moveTo(x1, y1) 
+  ctx.lineTo(x2, y2) 
+  ctx.stroke();
+}
+
+function drawText(ctx,text,x,y){  // draw a single text
+  ctx.fillText(text,x,y)
+}
 
 function drawWordLines(data_rows,canvas_layer){
   ctx = pc.ctx[canvas_layer]
-  x=100,y=2
+  x1=100,y1=2, x2= pc.position("gender")
   data_rows.forEach(function(row){
-    row['x'] = x
-    row['y'] = y 
-    drawLine(ctx, x+5, wordAxis(row['word']), pc.position("gender"), pc.dimensions()["gender"].yscale(row["gender"]))
-    y += 15
+    drawLine(ctx, x1+5, wordAxis(row['word']), x2, pc.dimensions()["gender"].yscale(row["gender"]))
+    // y1 += 15
   })
 }
 function drawWords(data_rows, canvas_layer) {
@@ -39,18 +113,14 @@ function drawWords(data_rows, canvas_layer) {
   x = (hidAxis) ? pc.position("gender")-5 : 100
     
   data_rows.forEach(function(row){
-    ctx.fillText(row['word'], x, pc.dimensions()["gender"].yscale(row["gender"]))
-  })
-  
+    drawText(ctx,row['word'], x, pc.dimensions()["gender"].yscale(row["gender"]))
+  }) 
 }
 
-function drawLine(ctx,x1,y1,x2,y2){
-  ctx.beginPath();
-  ctx.moveTo(x1, y1) 
-  ctx.lineTo(x2, y2) 
-  ctx.stroke();
-}
-function addLabels(data_rows,cls="labels"){
+/*
+SVG Manipulations
+*/
+function addSVGLabels(data_rows,cls="dynamicLabels"){ //dynamically add labels on the word axis
   d3.select("#word_axis")
     .selectAll(cls)
       .data(data_rows).enter()
@@ -63,3 +133,77 @@ function addLabels(data_rows,cls="labels"){
         .style("z-index","-4")
         .text(d => d.word)
 }
+
+/* 
+events associated with word axis
+*/
+$("body").on("mouseenter","#word_axis .tick text",function(e){
+  if(!inSearch){
+    $(this).addClass("focused")
+    $(this).attr("fill","#43a2ca")
+    // $(".labels").attr("opacity","0.1")
+    highlightWords($(this).html())
+  }
+  
+})
+
+$("body").on("mouseleave","#word_axis .tick text",function(){
+  if(!inSearch)
+    cancelHighlight() 
+})
+
+$("body").on("click","#canvas_svg",function(e){
+    console.log(e.target.nodeName)
+    if(e.target.nodeName == "text" && ($(e.target).parents(".tick").length) && ($(e.target).parents("#word_axis").length)){
+      console.log("enterr")
+      inSearch = true
+      searchWords($(e.target).html())
+      // $("#word_dimension .tick text").attr("opacity","0.0")
+    }
+    else if(!$(e.target).hasClass("dynamicLabel")){
+      inSearch = false
+      cancelHighlight()
+    }
+})
+
+$("body").on("mouseenter",".dynamicLabel",function(){
+  $(".dynamicLabel").not(this).attr("opacity","0.5")
+  afterHighlight = true
+  globalY = parseFloat($(this).attr("y"))
+  highlightWords($(this).html())
+})
+$("body").on("mouseleave",".dynamicLabel",function(){
+  $(".dynamicLabel").attr("opacity","1")
+  cancelHighlight()
+})
+$("body").on("mouseenter",".list-group-item",function(e){
+  $(this).css("color","orange")
+  word = $(this).html()
+  wordLabel = $(".dynamicLabel").filter(function(){
+    // console.log(d)
+    return $(this).html() == word
+  })
+  // console.log(wordLabel)
+  $(".dynamicLabel").not(wordLabel).attr("opacity","0.5")
+  afterHighlight = true
+  globalY = parseFloat(wordLabel.attr("y"))
+  highlightWords(word)
+})
+$("body").on("mouseleave",".list-group-item",function(e){
+  $(this).css("color","black")
+  $(".dynamicLabel").attr("opacity","1")
+})
+
+/*
+Search events
+*/
+$("body").on("mouseover",".result",function(){
+  inSearch = false
+  word = $(this).find(".title").html()
+  // $("#word_axis .tick text").attr("opacity","0.0")
+  highlightWords(word)
+})
+
+//$("body").on("mouseout",".result",function(){
+//  cancelHighlight()  
+//})
