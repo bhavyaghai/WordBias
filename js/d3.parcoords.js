3ea6495fa7518f6a53253960a50e19787e06571b
tonmoycsedu <tonmoycsedu@gmail.com>
2020-09-29 07:19:16 -0400

    added extra margin on left

--- js/d3.parcoords.js
+++ js/d3.parcoords.js
@@ -11,7 +11,7 @@ d3.parcoords = function(config) {
     rate: 20,
     width: 600,
     height: 300,
-    margin: { top: 24, right: 0, bottom: 12, left: 0 },
+    margin: { top: 24, right: 0, bottom: 12, left: 20 },
     nullValueSeparator: "undefined", // set to "top" or "bottom"
     nullValueSeparatorPadding: { top: 8, right: 0, bottom: 8, left: 0 },
     //color: "#069",
